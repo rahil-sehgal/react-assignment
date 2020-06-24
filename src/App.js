60faa0456a8be3993b9ada76536a74af39e551ba
Rahil Sehgal <rahilsehgal23@gmail.com>
2020-06-24 15:03:44 +0530

    Uncontrolled Form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <Main />
+      </div>
+    </BrowserRouter>
+    );
+  }
 }
+
+
+/* render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+         <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+  */
+
 
 export default App;
